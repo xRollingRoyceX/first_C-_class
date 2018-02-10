254bc4d0e43e1ba6d2f2045a71b68933018d7fdc
Sean <jesky.sean@outlook.com>
2018-02-10 10:18:24 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ void print_results()
         get_area *ptr1 = &a1;
 
         cout << "[+] length of the length:\t" << ptr1 -> l << endl;
-        cout << "[+] length of the height:\t" << ptr1 -> h << endl;
         cout << "[+] length of the width: \t" << ptr1 -> w << endl;
+        cout << "[+] length of the height:\t" << ptr1 -> h << endl;
         cout << "[+] Total Area: \t\t" << ptr1 -> a << endl;
 }
 int main()
