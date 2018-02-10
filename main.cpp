e7ab1f143179c54e4b99f0e2b5ba2e5184fb34f2
Sean <jesky.sean@outlook.com>
2018-02-10 10:12:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,17 @@
+
 #include "data_class"
+//..
+void print_results()
+{
+        get_area a1(14.21, 4.33, 44.3);
+        get_area *ptr1 = &a1;
 
-
+        cout << "[+] length of the length:\t" << ptr1 -> l << endl;
+        cout << "[+] length of the height:\t" << ptr1 -> h << endl;
+        cout << "[+] length of the width: \t" << ptr1 -> w << endl;
+        cout << "[+] Total Area: \t\t" << ptr1 -> a << endl;
+}
 int main()
 {
-	get_the c1(47.62352,0,72);
-	cout << c1.b << endl;
+        print_results();
 }
-#include <std_lib_facilities.h>
-
-class force_class{
-public:
-        double f, m, a;
-        force_class(double force, double mass, double accel) :
-                f(mass * accel),
-                m(force * accel),
-                a(mass * force) {}
-};
-class get_area
-{
-public:
-        double a = 0.0;
-        double l, w, h;
-        get_area(double l, double w, double h) :
-                //I gotta figure this out...
-                 a (l *  w * h ),//area
-                 l (a / (w * h)),//length
-                 w (a / (l * h)),//width
-                 h (a / (l * w)) /*height*/ {}
-                //I figured this out....
-};
-
