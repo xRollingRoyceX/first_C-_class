adef71cfc0e7884e2dd509c589ac1a6e36caaaf7
Sean <jesky.sean@outlook.com>
2018-02-10 10:16:30 -0700

    Create get_data.h

---
+++ get_data.h
@@ -0,0 +1,20 @@
+
+#include <std_lib_facilities.h>
+
+//omit file_name<<.>h> of this file if using not using raspberry PI or using nano
+//(omitting .h in nano makes things harder to read)
+#include <std_lib_facilities.h>
+class get_area
+{
+public:
+        double a = 0.0;
+        double l, w, h;
+
+        get_area(double l, double w, double h):
+                //I gotta figure this out...
+                 a (l *  w * h ),//area
+                 l (a / (w * h)),//length
+                 w (a / (l * h)),//width
+                 h (a / (l * w)) /*height*/ {}
+                //I figured this out....
+};
